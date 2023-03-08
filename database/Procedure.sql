7c263f5a05dd0fb03b1a3ed4d4eecf8b6be1150a
DimitrijeKucuk <d.kucuk98@gmail.com>
2023-03-08 10:31:18 +0100

    Create Procedure.sql

---
+++ database/Procedure.sql
@@ -0,0 +1,11 @@
+USE employee;
+GO
+CREATE PROCEDURE dbo.GetEmployeeWithPositionAndDate   
+    @PositionName nvarchar(50),   
+    @Date Date  
+AS   
+
+    SET NOCOUNT ON;  
+    SELECT * FROM EmployeePosition ep INNER JOIN JobPosition jp on ep.JobPositionId = jp.Id INNER JOIN Employee e on ep.EmployeeId = e.Id
+	WHERE ep.DateFrom <= @Date and ep.DateTo >= @Date and jp.PositionName = @PositionName
+GO
